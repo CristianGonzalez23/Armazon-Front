37f17734276f57bc11b07d69e9340a55f0a5c907
cristian <Cristianr.gonzalezi@uqvirtual.edu.co>
2023-05-03 17:15:05 -0500

    creacion de carpetas vistas

--- Armazon/src/app/app.module.ts
+++ Armazon/src/app/app.module.ts
@@ -6,13 +6,31 @@ import { AppComponent } from './app.component';
 import { InicioComponent } from './pagina/inicio/inicio.component';
 import { LoginComponent } from './pagina/login/login.component';
 import { RegistroComponent } from './pagina/registro/registro.component';
+import { FavoritoComponent } from './pagina/pagina/favorito/favorito.component';
+import { ProductoComponent } from './pagina/producto/producto.component';
+import { MyCartComponent } from './pagina/my-cart/my-cart.component';
+import { AdministradorComponent } from './pagina/administrador/administrador.component';
+import { ListaComprasComponent } from './pagina/lista-compras/lista-compras.component';
+import { ProductosEnVentaComponent } from './pagina/productos-en-venta/productos-en-venta.component';
+import { CiudadComponent } from './pagina/ciudad/ciudad.component';
+import { CreacionProductoComponent } from './pagina/creacion-producto/creacion-producto.component';
+import { RestablecerPasswordComponent } from './pagina/restablecer-password/restablecer-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     LoginComponent,
-    RegistroComponent
+    RegistroComponent,
+    FavoritoComponent,
+    ProductoComponent,
+    MyCartComponent,
+    AdministradorComponent,
+    ListaComprasComponent,
+    ProductosEnVentaComponent,
+    CiudadComponent,
+    CreacionProductoComponent,
+    RestablecerPasswordComponent
   ],
   imports: [
     BrowserModule,
